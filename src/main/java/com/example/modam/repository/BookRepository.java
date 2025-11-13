ee2109a6bd78b3acb0bf50f4f17707a47fdf0c7f
boyeonp <dnflwlq20020416@gmail.com>
2025-11-13 18:43:38 +0900

    fix: user,book 테이블 중복 제거

--- src/main/java/com/example/modam/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.modam.repository;
-
-import com.example.modam.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Book 엔티티와 해당 엔티티의 Primary Key 타입 지정
-public interface BookRepository extends JpaRepository<Book, Long>{
-    
-}
