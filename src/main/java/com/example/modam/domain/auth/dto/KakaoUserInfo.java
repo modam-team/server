652e0d991a0a2d7c797082ba5383a67b65746775
boyeonp <dnflwlq20020416@gmail.com>
2025-11-26 12:17:31 +0900

    fix: 이메일 제거

--- src/main/java/com/example/modam/domain/auth/dto/KakaoUserInfo.java
+++ src/main/java/com/example/modam/domain/auth/dto/KakaoUserInfo.java
@@ -13,25 +13,10 @@ public class KakaoUserInfo {
     // 카카오 회원 번호 (고유 ID)
     private Long id;
 
-    // 카카오 계정 정보
-    @JsonProperty("kakao_account")
-    private KakaoAccount kakaoAccount;
-
     // 프로필 정보 (닉네임, 이미지 등)
     @JsonProperty("properties")
     private UserProperties properties;
 
-    // 내부 클래스: 계정 정보
-    @Getter
-    @NoArgsConstructor
-    public static class KakaoAccount {
-
-        @JsonProperty("has_email")
-        private Boolean hasEmail;
-
-        @JsonProperty("email")
-        private String email;
-    }
 
     // 내부 클래스: 프로필 속성
     @Getter
