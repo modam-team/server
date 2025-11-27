c02c591a66ce10e757d20a51d030f58647812cb3
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-11-27 15:35:52 +0900

    feat: DTO에 코멘트, 상태 추가, 주석 작성

--- src/main/java/com/example/modam/domain/bookcase/Presentation/BookCaseInfoResponse.java
+++ src/main/java/com/example/modam/domain/bookcase/Presentation/BookCaseInfoResponse.java
@@ -1,11 +1,13 @@
 package com.example.modam.domain.bookcase.Presentation;
 
+import com.example.modam.domain.bookcase.Domain.BookState;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class BookCaseInfoResponse {
+    // BookInfoResponse에서 데이터를 가져옴
     private long bookId;
     private String title;
     private String author;
@@ -14,9 +16,15 @@ public class BookCaseInfoResponse {
     private String publisher;
     private double rate;
     private long totalReview;
+
+    // BookInfoEntity에서 데이터를 가져옴
+    private BookState status;
     private LocalDateTime enrollAt;
     private LocalDateTime startedAt;
     private LocalDateTime finishedAt;
+
+    // 완독한 유저만 이거 채움
     private double userRate;
     private String userHashTag;
+    private String userComment;
 }
