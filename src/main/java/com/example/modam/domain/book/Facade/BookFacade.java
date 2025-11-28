1a1c41264e077ba59b043cead66b3124cc38b1bd
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-11-28 21:16:15 +0900

    perf: 베스트셀러 서버 내부 캐싱

--- src/main/java/com/example/modam/domain/book/Facade/BookFacade.java
+++ src/main/java/com/example/modam/domain/book/Facade/BookFacade.java
@@ -5,6 +5,7 @@ import com.example.modam.domain.book.Presentation.BookInfoResponse;
 import com.example.modam.domain.book.Application.BookService;
 import com.example.modam.domain.book.Domain.BookEntity;
 import com.example.modam.domain.book.Presentation.BookSearchRequest;
+import com.example.modam.global.utils.BestSellerCache;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,19 +16,46 @@ import java.util.stream.Collectors;
 public class BookFacade {
     private final BookService bookService;
     private final BookDataService bookDataService;
+    private final BestSellerCache bestSellerCache;
 
-    public BookFacade(BookService bookService, BookDataService bookDataService) {
+    public BookFacade(BookService bookService, BookDataService bookDataService, BestSellerCache bestSellerCache) {
         this.bookService = bookService;
         this.bookDataService = bookDataService;
+        this.bestSellerCache = bestSellerCache;
     }
 
-    // 알라딘 검색한 스레드가 이어서 DB에 저장하도록 연결하는 퍼사드
+    // 알라딘 검색한 스레드가 이어서 DB에 저장하도록 연결하는 퍼사드, 베스트셀러는 서버 캐시에 저장
     public CompletableFuture<List<BookInfoResponse>> searchBook(BookSearchRequest dto) throws Exception {
-        return bookService.parseBookData(dto).thenApply(bookData -> {
-            List<BookEntity> entities = bookDataService.saveBook(bookData);
-            return entities.stream()
-                    .map(bookDataService::toDto)
-                    .collect(Collectors.toList());
-        });
+
+        boolean isBestseller = "Bestseller".equals(dto.getQueryType());
+        if (isBestseller) {
+            CompletableFuture<List<BookInfoResponse>> cached = bestSellerCache.get();
+            if (cached != null) {
+                return cached;
+            }
+        }
+
+        CompletableFuture<List<BookInfoResponse>> response =
+                bookService.parseBookData(dto)
+                        .thenApply(bookData -> {
+                            List<BookEntity> entities = bookDataService.saveBook(bookData);
+                            return entities.stream()
+                                    .map(bookDataService::toDto)
+                                    .collect(Collectors.toList());
+                        });
+
+        if (isBestseller) {
+            synchronized (bestSellerCache) {
+                if (bestSellerCache.isExist()) {
+                    CompletableFuture<List<BookInfoResponse>> already = bestSellerCache.get();
+                    if (already != null) {
+                        return already;
+                    }
+                }
+                bestSellerCache.saveFuture(response);
+            }
+        }
+
+        return response;
     }
 }
