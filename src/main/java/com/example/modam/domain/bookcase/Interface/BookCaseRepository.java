eef8107bf99cb3ed019039690abcc186a4fc75c8
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-12-08 14:34:32 +0900

    feat: 유저 관심 카테고리 & 책장에 없는 책 탐색 쿼리 작성

--- src/main/java/com/example/modam/domain/bookcase/Interface/BookCaseRepository.java
+++ src/main/java/com/example/modam/domain/bookcase/Interface/BookCaseRepository.java
@@ -12,8 +12,6 @@ import java.util.Optional;
 public interface BookCaseRepository extends JpaRepository<BookCaseEntity, Long> {
     boolean existsByUser_IdAndBook_Id(Long userId, Long bookId);
 
-    List<BookCaseEntity> findByUser_Id(Long userId);
-
     Optional<BookCaseEntity> findByUser_IdAndBook_Id(Long userId, Long bookId);
 
     @Query(value = """
