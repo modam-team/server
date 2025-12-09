031e515f90d9f9cf6aca7e5bec3b6b08d6749735
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-12-09 18:56:03 +0900

    feat: 리포트의 외부 스키마 쿼리 작성

--- src/main/java/com/example/modam/domain/report/Interface/ReportRepository.java
+++ src/main/java/com/example/modam/domain/report/Interface/ReportRepository.java
@@ -2,6 +2,7 @@ package com.example.modam.domain.report.Interface;
 
 import com.example.modam.domain.report.Domain.ReadingLogEntity;
 import com.example.modam.domain.report.Presentation.dto.ReadingLogResponse;
+import com.example.modam.domain.report.Presentation.dto.ReportResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -24,4 +25,19 @@ public interface ReportRepository extends JpaRepository<ReadingLogEntity, Long> 
             """)
     List<ReadingLogResponse> findByDate(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end,
                                         @Param("userId") long userId);
+
+    @Query("""
+            select new com.example.modam.domain.report.Presentation.dto.ReportResponse(
+            r.readAt,r.readingPlace,b.categoryName,string_agg(ht.tag, ' ')
+            )
+            from reading r
+            join r.bookCase bc
+            join bc.user u
+            join bc.book b
+            left join review rev on rev.bookCase=bc
+            left join rev.hashtags ht
+            where u.id=:userId
+            group by r.readAt, r.readingPlace, b.categoryName
+            """)
+    List<ReportResponse> findReportData(@Param("userId") long userId);
 }
