852f24de82f03e5fe8dd81d9359f35b520c47fbc
boyeonp <dnflwlq20020416@gmail.com>
2026-01-04 22:10:56 +0900

    feat: 테마 색상 변경 예외 처리

--- src/main/java/com/example/modam/domain/user/Domain/UserEntity.java
+++ src/main/java/com/example/modam/domain/user/Domain/UserEntity.java
@@ -1,5 +1,7 @@
 package com.example.modam.domain.user.Domain;
 
+import com.example.modam.global.exception.ApiException;
+import com.example.modam.global.exception.ErrorDefine;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -74,6 +76,17 @@ public class UserEntity {
         }
         if (goalScore!=null){
             this.goalScore = goalScore;
+        }
+    }
+
+    public void updateThemeColor(String themeColor){
+        if (themeColor == null || themeColor.isBlank()){
+            throw new ApiException(ErrorDefine.INVALID_THEME_COLOR);
+        }
+        if (!themeColor.startsWith("#")){
+            this.themeColor = "#" + themeColor;
+        } else {
+            this.themeColor = themeColor;
         }
     }
 
